6b12cca3716ed48018c88bb5e92c1dbaace8cf55
André Ricardo <andric87@gmail.com>
2024-04-25 21:42:06 +0100

    Fix Typescript error.

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -5,17 +5,15 @@ interface IExampleProps {
   resetChoice?: () => void;
 }
 
-function randomEnum<T>(anEnum: T): T[keyof T] {
-  const enumValues = (Object.keys(anEnum)
-    .map((n) => Number.parseInt(n))
-    .filter((n) => !Number.isNaN(n)) as unknown) as T[keyof T][];
-  const randomIndex = Math.floor(Math.random() * enumValues.length);
-  const randomEnumValue = enumValues[randomIndex];
-  return randomEnumValue;
+function randomEnumValue<T extends object>(enumObj: T): T[keyof T] {
+  const enumValues = Object.values(enumObj);
+  const index = Math.floor(Math.random() * enumValues.length);
+  
+  return enumValues[index];
 }
 
 function _randomMessageBarType(): MessageBarType {
-  const randomMessageBarEnum = randomEnum(MessageBarType);
+  const randomMessageBarEnum = randomEnumValue(MessageBarType);
   return randomMessageBarEnum;
 }
 
