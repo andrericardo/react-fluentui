d2b414aac7e5e528ed16a6c2cfe954884d573bfe
André Ricardo <andric87@gmail.com>
2024-04-28 19:18:04 +0100

    Fix warning React state expects updates wrapped in act.
    "When testing, code that causes React state updates should be wrapped into act(...)"

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,28 +1,30 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
+    it('true to be true', () => {
+        expect(true).toBe(true);
+    });
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
+    it('false to be false', () => {
+        expect(false).toBe(false);
+    });
 });
 
 
-import {App} from './App';
+import { App } from './App';
 
 describe('App', () => {
-  it('renders product name', () => {
-    render(<App />);
+    it('renders product name', async () => {
+        render(<App />);
 
-    // screen.debug();
+        // screen.debug();
 
-    // check if App components renders headline
-    const linkElement = screen.getByText(/Product name/i);
-    
-    expect(linkElement).toBeInTheDocument();
-  });
+        // check if App components renders headline
+        await waitFor(() => {
+            const linkElement = screen.getByText(/Product name/i);
+
+            expect(linkElement).toBeInTheDocument();
+        });
+    });
 });
